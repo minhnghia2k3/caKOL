180c36136f2b4f8c38bb214106f4749151c360b1
angutboiz <thngan25k3@gmail.com>
2024-01-16 21:15:53 +0700

    add UI header, main, footer but uncompleted

---
+++ app/components/footer.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <div id="nc-footer-fixed-area" className="fixed bottom-0 inset-x-0 flex flex-col-reverse z-40">
+            <div className="FooterQuickNav z-40 flex-1 w-full block xl:hidden p-2 bg-white dark:bg-neutral-800 border-t border-neutral-300 dark:border-neutral-700 transition-transform duration-300 ease-in-out">
+                <div className="w-full max-w-screen-sm lg:max-w-screen-md flex justify-around mx-auto text-2xl text-center ">
+                    <a href="https://bookkol.com/" className="FooterQuickNav__home flex flex-col items-center justify-between text-neutral-900 dark:text-neutral-50">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 sm:w-6 sm:h-6">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                            ></path>
+                        </svg>
+                        <span className="text-[10px] leading-none mt-1">Trang chủ </span>
+                    </a>
+
+                    <a href="https://bookkol.com/?s=" className="FooterQuickNav__search flex flex-col items-center justify-between text-neutral-500 dark:text-neutral-300/90">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 sm:w-6 sm:h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                        </svg>
+                        <span className="text-[10px] leading-none mt-1">Tìm kiếm </span>
+                    </a>
+
+                    <button type="button" className="FooterQuickNav__menu btn-toogle-mobile-sidebar-menu flex flex-col items-center justify-between text-neutral-500 dark:text-neutral-300/90 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 sm:w-6 sm:h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+                        </svg>
+                        <span className="text-[10px] leading-none mt-1">Danh mục </span>
+                    </button>
+                </div>
+            </div>
+            <button
+                className="ScrollTop absolute bottom-full right-0 z-30 flex-shrink-0 mb-2.5 mr-2.5 md:mb-5 md:mr-5 w-10 h-10 bg-black/10 
+      hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20 
+      items-center justify-center rounded-full text-xl transition-opacity text-white dark:text-neutral-300"
+                style={{ display: "none" }}
+            >
+                <i className="las la-arrow-up"></i>
+            </button>
+        </div>
+    )
+}
---
+++ app/components/header.tsx
@@ -0,0 +1,86 @@
+import Link from "next/link"
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="container flex ">
+            <div className="relative flex justify-between space-x-4 xl:space-x-8">
+                <div className="flex justify-start flex-1 space-x-4 sm:space-x-6 2xl:space-x-9 ">
+                    <div className="site-logo text-4xl">
+                        <Link href="/">
+                            <h2 className="font-bold text-[#e19a48]">
+                                ca<span className="text-primary">KOL</span>
+                            </h2>
+                        </Link>
+                    </div>
+
+                    <nav id="site-navigation" className="hidden xl:block h-full primary-navigation site-header-nav-main flex-grow" aria-label="Primary menu">
+                        <div className="primary-menu-container w-full h-full">
+                            <ul id="primary-menu-list" className="nc-Navigation flex lg:flex-wrap h-full lg:space-x-1 menu-wrapper justify-center items-center">
+                                <li className="">
+                                    <Link href="https://bookkol.com/" className="menu-link main-menu-link">
+                                        Trang Chủ
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="#" className="menu-link main-menu-link">
+                                        Về bookKOL
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="https://bookkol.com/dich-vu-kol-marketing/" className="menu-link main-menu-link">
+                                        Dịch Vụ
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="https://bookkol.com/danh-sach-kols/" className="menu-link main-menu-link">
+                                        Danh sách KOLs
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="/bao-gia-kol/" className="menu-link main-menu-link">
+                                        Báo Giá KOL
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="/blog/" className="menu-link main-menu-link">
+                                        Blog
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="/lien-he-bookkol" className="menu-link main-menu-link">
+                                        Liên Hệ
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div className="py-1.5 xl:py-3.5 flex-shrink-0 flex justify-end text-neutral-700 dark:text-neutral-100">
+                        <div className="hidden xl:block">
+                            <div data-is-react-component="SearchDropdown">
+                                <button
+                                    type="button"
+                                    className="text-[28px] md:text-[28px] h-10 w-10 sm:w-12 sm:h-12 rounded-full text-neutral-700 dark:text-neutral-300 sm:hover:bg-neutral-100 sm:dark:hover:bg-neutral-800 focus:outline-none flex items-center justify-center"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="w-6 h-6 sm:w-7 sm:h-7">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="hidden xl:block"></div>
+
+                        <a
+                            data-ncmaz-open-modal="ncmaz-modal-form-sign-in"
+                            className="nc-Button ml-3 relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm xl:text-base xl:font-medium py-2 px-4 xl:py-3 xl:px-6 ttnc-ButtonPrimary bg-primary-6000 hover:bg-primary-700 text-neutral-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
+                            href="https://bookkol.com/wp-login.php?redirect_to=https%3A%2F%2Fbookkol.com"
+                            alt="Login"
+                        >
+                            Sign up{" "}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
