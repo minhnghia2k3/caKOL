37c3f38ae841cb029b58de0f422e4d2291c0f17f
minhnghia2k3 <BrozennVN@gmail.com>
2024-03-11 15:35:38 +0700

    feat: Added authentication page

---
+++ app/(app)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Header from "../components/header";
+import Main from "../components/main";
+import Footer from "../components/footer";
+import seed from "@/scripts/seed";
+export default async function Home() {
+  // await seed(); // Seed user data
+
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
