2d27bb31f85e94b2267a6727778134e05466438a
angutboiz <troandev@gmail.com@gmail.com>
2024-03-11 15:40:37 +0700

    feat(conpleted)

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,80 +1,116 @@
-"use client"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import React from "react"
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function Header() {
-    const path = usePathname()
+    const path = usePathname();
     return (
-        <div className="container ">
-            <div className="flex items-center justify-between space-x-4 xl:space-x-8">
+        <div className="px-5 sm:px-10 xl:px-20">
+            <div className="flex items-center justify-between">
                 <div className="flex justify-between items-center" style={{ width: "100%" }}>
                     <div className="site-logo text-4xl">
                         <Link href="/">
-                            <h2 className="font-bold text-[#e19a48]">
-                                ca<span className="text-primary">KOL</span>
+                            <h2 className="font-bold text-black">
+                                ca<span className="text-[#e0752b]">KOL</span>
                             </h2>
                         </Link>
                     </div>
-
-                    <nav id="site-navigation" className="hidden xl:block h-full primary-navigation site-header-nav-main flex-grow">
-                        <div className="primary-menu-container w-full h-full">
-                            <ul id="primary-menu-list" className="flex lg:flex-wrap h-full lg:space-x-1 menu-wrapper items-center justify-evenly">
-                                <li className="">
-                                    <Link href="/" className={`menu-link main-menu-link ${path === "/" ? "active" : ""}`}>
-                                        Trang Chủ
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/about" className="menu-link main-menu-link">
-                                        Về bookKOL
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/dichvu" className="menu-link main-menu-link">
-                                        Dịch Vụ
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/danhsachkol" className="menu-link main-menu-link">
-                                        Danh sách KOLs
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/bao-gia-kol/" className="menu-link main-menu-link">
-                                        Báo Giá KOL
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/blog/" className="menu-link main-menu-link">
-                                        Blog
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/lien-he-bookkol" className="menu-link main-menu-link">
-                                        Liên Hệ
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <div className="py-1.5 xl:py-3.5 flex-shrink-0 flex justify-end text-neutral-700 dark:text-neutral-100">
-                        <div className="hidden xl:block">
-                            <div data-is-react-component="SearchDropdown" className="mr-2">
-                                <button
-                                    type="button"
-                                    className="text-[28px] md:text-[28px] h-10 w-10 sm:w-12 sm:h-12 rounded-full text-neutral-700  sm:hover:bg-neutral-100 sm:dark:hover:bg-neutral-300 focus:outline-none flex items-center justify-center"
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" className="w-6 h-6 sm:w-7 sm:h-7">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                    <div className="w-[100%] px-20 my-2 md:block hidden">
+                        <form className="">
+                            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-black sr-only ">
+                                Search
+                            </label>
+                            <div className="relative">
+                                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                    <svg className="w-4 h-4 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                     </svg>
+                                </div>
+                                <input
+                                    type="search"
+                                    id="default-search"
+                                    className="block w-full p-4 ps-10 text-sm text-black border border-black rounded-lg "
+                                    placeholder="Search everything..."
+                                    required
+                                />
+                                <button
+                                    type="submit"
+                                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                >
+                                    Search
                                 </button>
                             </div>
+                        </form>
+
+                        <div className="mt-0 xl:mt-3">
+                            <nav id="site-navigation" className="hidden xl:block h-full primary-navigation site-header-nav-main flex-grow">
+                                <div className="primary-menu-container w-full h-full">
+                                    <ul id="primary-menu-list" className="flex lg:flex-wrap h-full lg:space-x-1 menu-wrapper items-center justify-evenly">
+                                        <li className="">
+                                            <Link href="/" className={`menu-link main-menu-link ${path === "/" ? "active" : ""}`}>
+                                                Trang Chủ
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link href="/about" className="menu-link main-menu-link">
+                                                Về bookKOL
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link href="/dichvu" className="menu-link main-menu-link">
+                                                Dịch Vụ
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link href="/danhsachkol" className="menu-link main-menu-link">
+                                                Danh sách KOLs
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link href="/bao-gia-kol/" className="menu-link main-menu-link">
+                                                Báo Giá KOL
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link href="/blog/" className="menu-link main-menu-link">
+                                                Blog
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link href="/lien-he-bookkol" className="menu-link main-menu-link">
+                                                Liên Hệ
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
                         </div>
-                        <button className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full">Button</button>
+                    </div>
+                    <div className="py-1.5 xl:py-3.5 flex-shrink-0 flex justify-end text-neutral-700 dark:text-neutral-100">
+                        <button className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full">Sign up</button>
                     </div>
                 </div>
             </div>
+            <form className="md:hidden block my-3">
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-black sr-only ">
+                    Search
+                </label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-black border border-black rounded-lg " placeholder="Search everything..." required />
+                    <button
+                        type="submit"
+                        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Search
+                    </button>
+                </div>
+            </form>
         </div>
-    )
+    );
 }
--- app/components/main.tsx
+++ app/components/main.tsx
@@ -11,31 +11,29 @@ export default async function Main() {
     return (
         <div className="">
             <div className="image-main overflow-hidden flex items-center justify-center">
-                <div className="left text-center ">
-                    <h1 className="font-bold text-4xl">Nền tảng LOL Marketing Chuyên Nghiệp</h1>
-                    <p className="my-10 text-xl">
-                        Với kinh nghiệm triển khai 1000+ dự án, book KOL chắc chắn sẽ <br /> mang đến cho quý khách hàng trải nghiệm dịch vụ tốt nhất
-                    </p>
+                <div className="left text-center px-10 ">
+                    <h1 className="font-bold sm:text-4xl text-xl">Nền tảng LOL Marketing Chuyên Nghiệp</h1>
+                    <p className="my-10 sm:text-xl">Với kinh nghiệm triển khai 1000+ dự án, book KOL chắc chắn sẽ mang đến cho quý khách hàng trải nghiệm dịch vụ tốt nhất</p>
                     <button className="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
                         <a href="">Tư vấn ngay</a>
                     </button>
                 </div>
-                <div className="right">
+                <div className="right sm:block hidden">
                     <Image src="/asstes/model-bookkol.png" alt="" width={370} height={370}></Image>
                 </div>
             </div>
-            <div className="mx-20 my-10 ">
-                <div className="about-booking flex gap-10 items-center jutify-center">
-                    <div className="w-2/3">
+            <div className="mx-2 my-20 sm:mx-20 sm:my-10 ">
+                <div className="about-booking flex gap-10 items-center jutify-center flex-col sm:flex-row">
+                    <div className="sm:w-2/3">
                         <Image src={"/asstes/bookingkol.jpg"} alt="" width={600} height={600}></Image>
                     </div>
-                    <div className="w-2/3 ">
+                    <div className="sm:w-2/3 ">
                         <h2 className={`${kaushan.className} font-[700] text-[#d25a1b] text-3xl`}>About Booking 👇</h2>
                         <p className="text-xl my-5 font-[500]">
                             Là nền tảng <label className="text-[#d25a1b] font-[600]"> KOL Marketing</label> hàng đầu hiện nay, đứng sau thành công của nhiều chiến dịch lớn nhỏ. Vậy tại sao bookKOL lại
                             được các thương hiệu tín nhiệm đến vậy?
                         </p>
-                        <ol className="text-xl">
+                        <ol className="sm:text-xl">
                             <li className="mb-5">1. Có 50.000+ KOL trong Network</li>
                             <li className="mb-5">2. Sở hữu cộng đồng 175.000+ thành viên</li>
                             <li className="mb-5">3. Là Agency booking KOL uy tín lâu năm</li>
@@ -48,23 +46,23 @@ export default async function Main() {
                     </div>
                 </div>
             </div>
-            <div className="element-number p-10 pt-20 bg-black text-white ">
-                <div className={`${rajd.className} text-3xl flex gap-5 justify-between text-center`}>
-                    <div className="">
-                        <h3 className="text-[80px]">50,000+</h3>
-                        <p className="mt-10 text-[#e19a48]">KOLS</p>
+            <div className="element-number sm:p-10 sm:pt-20 bg-black text-white">
+                <div className={`${rajd.className} sm:text-3xl grid grid-rows-2 grid-flow-col sm:flex gap-2 justify-center md:justify-evenly items-center px-2`}>
+                    <div className="text-center">
+                        <h3 className="text-2xl md:text-[80px]">50,000+</h3>
+                        <p className="sm:mt-10 text-[12px] text-[#e19a48]">KOLS</p>
                     </div>
-                    <div className="">
-                        <h3 className="text-[80px]">175000+</h3>
-                        <p className="mt-10 text-[#e19a48]">MEMBERSHIP</p>
+                    <div className="text-center">
+                        <h3 className="text-2xl md:text-[80px]">175000+</h3>
+                        <p className="sm:mt-10 text-[12px] text-[#e19a48]">MEMBERSHIP</p>
                     </div>
-                    <div className="">
-                        <h3 className="text-[80px]">1532</h3>
-                        <p className="mt-10 text-[#e19a48]">PROJECT</p>
+                    <div className="text-center">
+                        <h3 className="text-2xl md:text-[80px]">1532</h3>
+                        <p className="sm:mt-10 text-[12px] text-[#e19a48]">PROJECT</p>
                     </div>
-                    <div className="">
-                        <h3 className="text-[80px]">1168</h3>
-                        <p className="mt-10 text-[#e19a48]">HAPPY CLIENTS</p>
+                    <div className="text-center">
+                        <h3 className="text-2xl md:text-[80px]">1168</h3>
+                        <p className="sm:mt-10 text-[12px] text-[#e19a48]">HAPPY CLIENTS</p>
                     </div>
                 </div>
             </div>
@@ -87,10 +85,10 @@ export default async function Main() {
                     </p>
                 </div>
             </div>
-            <div className="mt-20 flex items-center justify-center">
-                <div className="text-center w-[80%] ">
-                    <div className="flex items-center justify-between gap-20">
-                        <div className="w-[50%]">
+            <div className="mt-10 flex items-center justify-center p-5">
+                <div className="text-center md:w-[80%] ">
+                    <div className="md:flex items-center justify-between gap-20">
+                        <div className="md:w-[50%]">
                             <h1 className="text-3xl text-[#e0752b] font-bold">CÂU HỎI THƯỜNG GẶP</h1>
                             <p className="my-10 text-xl">
                                 <label className="text-[#e0752b] font-bold">KOL Marketing </label> là hình thức doanh nghiệp sử dụng <label className="text-[#e0752b] font-bold">KOL</label> trong kế
@@ -100,7 +98,7 @@ export default async function Main() {
                                 <label className="text-[#e0752b] font-bold">KOL</label> tìm hiểu thêm về hình thức quảng cáo đầy tiềm năng này.
                             </p>
                         </div>
-                        <div className="text-left w-[50%] text-xl">
+                        <div className="text-left md:w-[50%] text-xl">
                             <div className="mb-5 border-t-2 py-3">KOC là gì?</div>
                             <div className="mb-5 border-t-2 py-3">Booking KOL là gì?</div>
                             <div className="mb-5 border-t-2 py-3">KOL Marketing là gì?</div>
